7b99585a539602a92c4e02d8307662e46797bd70
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-07-28 21:39:30 +0300

    all photos added and create card components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CardProject';
+  posts=[
+    {
+    title: 'Köpek',
+    imageUrl: 'assets/foto1.jpg',
+    username: '@hayvan',
+    content: 'Patili dostumuz.'
+  },
+  {
+    title: 'Dağ',
+    imageUrl: 'assets/foto2.jpg',
+    username: '@tırmanış',
+    content: 'Doğa yürüyüşü.'
+  },
+  {
+    title: 'Kule',
+    imageUrl: 'assets/foto3.jpg',
+    username: '@kule',
+    content: 'Fener kulesi.'
+  }
+]
 }
